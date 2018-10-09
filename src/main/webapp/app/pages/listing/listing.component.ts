45fd9cc501c4f900f8d9290cfee38d5807a50cff
shashankpenumatcha <shashankpenumatcha@gmail.com>
2018-10-10 00:44:54 +0530

    get product api integrated

--- src/main/webapp/app/pages/listing/listing.component.ts
+++ src/main/webapp/app/pages/listing/listing.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { NgbModalRef } from '@ng-bootstrap/ng-bootstrap';
 import { JhiEventManager } from 'ng-jhipster';
+import { HttpClient, HttpResponse } from '@angular/common/http';
 
 @Component({
     selector: 'jhi-listing',
@@ -8,6 +9,20 @@ import { JhiEventManager } from 'ng-jhipster';
     styleUrls: ['listing.css']
 })
 export class ListingComponent implements OnInit {
-   constructor() {}
-    ngOnInit() {}
+    products: any[];
+    loading = true;
+    constructor(private http: HttpClient) {}
+    ngOnInit() {
+        this.http.get<any>('api/activeProducts', { observe: 'response' }).subscribe(
+            (res: any) => {
+                if (res.body !== undefined && res.body.length > 0) {
+                    this.products = res.body;
+                    this.loading = false;
+                }
+            },
+            (error: any) => {
+                this.loading = false;
+            }
+        );
+    }
 }
